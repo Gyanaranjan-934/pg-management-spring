57bd3eab6bf9a1d848a188e64a099c74df197355
Gyanaranjan Sahoo <gyanaranjansahoo509@gmail.com>
2025-12-27 23:33:25 +0530

    feat: add bed service

--- src/main/java/com/gyan/pg_management/repository/RoomRepository.java
+++ src/main/java/com/gyan/pg_management/repository/RoomRepository.java
@@ -2,6 +2,7 @@ package com.gyan.pg_management.repository;
 
 import com.gyan.pg_management.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
