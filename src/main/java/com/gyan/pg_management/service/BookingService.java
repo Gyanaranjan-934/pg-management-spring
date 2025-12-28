877ecf0e5fd2cde7bd9e21de71935bc195b49a37
Gyanaranjan Sahoo <gyanaranjansahoo509@gmail.com>
2025-12-28 13:24:57 +0530

    feat: add checkout booking serive method

--- src/main/java/com/gyan/pg_management/service/BookingService.java
+++ src/main/java/com/gyan/pg_management/service/BookingService.java
@@ -3,6 +3,7 @@ package com.gyan.pg_management.service;
 import com.gyan.pg_management.entity.*;
 import com.gyan.pg_management.enums.BookingStatus;
 import com.gyan.pg_management.repository.BookingRepository;
+import com.gyan.pg_management.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 public class BookingService {
 
     private final BookingRepository bookingRepository;
+    private final PaymentRepository paymentRepository;
 
     public Booking createBooking(
             Tenant tenant,
@@ -44,4 +46,32 @@ public class BookingService {
 
         return bookingRepository.save(booking);
     }
+
+    public Booking checkoutBooking(Long bookingId) {
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new IllegalArgumentException("Booking not found"));
+
+        if (booking.getStatus() == BookingStatus.COMPLETED) {
+            throw new IllegalStateException("Booking already completed");
+        }
+
+        double totalPaid = paymentRepository.findByBooking(booking)
+                .stream()
+                .mapToDouble(Payment::getAmount)
+                .sum();
+
+        double pending = booking.getMonthlyRent() - totalPaid;
+
+        if (pending > 0) {
+            throw new IllegalStateException(
+                    "Cannot checkout. Pending dues: " + pending
+            );
+        }
+
+        booking.setStatus(BookingStatus.COMPLETED);
+        booking.setEndDate(LocalDate.now());
+
+        return bookingRepository.save(booking);
+    }
+
 }
